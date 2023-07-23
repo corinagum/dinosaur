3ce4951860f722a60d116de78aa0f43898cc7f28
Corina <14900841+corinagum@users.noreply.github.com>
2023-07-23 14:46:59 -0700

    frontend dump

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,11 +9,8 @@ function App() {
       <h1>Dinosaur</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <LyricsHOC />
     </>
   );
 }
